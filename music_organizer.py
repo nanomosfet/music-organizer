50ef9ae501ea069b443edb004906155c29b1899f
nanomosfet <timmyzsearcy@gmail.com>
2017-11-14 14:58:26 -0800

    Updating for new command to create playlist from song bank

--- music_organizer.py
+++ music_organizer.py
@@ -1,10 +1,15 @@
 import os
 import datetime
 import argparse
+import shutil
 
 def write_log(s):    
     with open('logfile.out', 'a+') as f:
         f.write('time: %s Action: %s \n' % (str(datetime.datetime.now()), s))
+
+def write_report(path ,s):    
+    with open('%s/report.out' % path, 'a+') as f:
+        f.write('%s\n' % s)
 
 
 def organize_files_by_artist(src, dst):
@@ -24,13 +29,64 @@ def organize_files_by_artist(src, dst):
                     write_log('Error occured moving  %s to %s' % (os.path.join(root, file), os.path.join(dst, band_name, file)))
                     write_log('Error is %s' % error)
 
+def clean_string(s):
+    s = ''.join([i if ord(i) < 128 else '' for i in s])
+    s.replace(' .mp3', '.mp3').replace('\'', '').replace('"', '')
+    return s
+
+def create_list_from_csv(list_csv):
+    res = []
+    with open(list_csv) as f:
+        for line in f:
+            if line == ',':
+                pass
+            else:
+                res.append(clean_string(line).replace('\t', ' - ').replace('\n', '.mp3'))
+    return res
+
+def create_playlist_from_bank(playlist_csv, src, dst):
+    try:        
+        playlist = create_list_from_csv(playlist_csv)
+    except:
+        print 'Error Reading csv'
+        write_log('Error Reading_csv')
+
+    try:
+        os.mkdir(dst)
+    except OSError:
+        print "Output File already Exists, Will Continue to put files in this directory "
+        write_log("Output File already Exists , Will Continue to put files in this directory")
+
+    if not os.path.exists(src):
+        print 'File does not exist'
+        write_log('Source File does not exist')
+    else:
+        for root, dirnames, filenames in os.walk(src):
+            for file in filenames:
+                file_cleaned = clean_string(file)
+                if file_cleaned in playlist:
+                    shutil.copy(os.path.join(root, file), os.path.join(dst, file))
+                    playlist.remove(file_cleaned)
+    print "the following files have not been found in your song bank:"
+    write_report(dst, "the following files have not been found in your song bank:")
+    for item in playlist:
+        print item
+        write_report(dst, item)
+    write_report(dst, "There are %s not found in your music bank" % len(playlist))
+    return playlist
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Organize Music files in a folder to a destination folder')
+    parser.add_argument('cmd', type=str, nargs=1)
     parser.add_argument('source', type=str, nargs=1)
+    parser.add_argument('-f', '--file', type=str, nargs=1)
     parser.add_argument('destination', type=str, nargs=1)
     args = parser.parse_args()
-    organize_files_by_artist(args.source[0], args.destination[0])
+    if args.cmd[0] == 'organize':
+        organize_files_by_artist(args.source[0], args.destination[0])
+    elif args.cmd[0] == 'make_playlist':
+        create_playlist_from_bank(args.file[0], args.source[0], args.destination[0])
+
 
--- tests.py
+++ tests.py
@@ -8,10 +8,10 @@ import music_organizer
 
 music_names = [
             'Beatles - P.S I Love you.mp3', 
-            'Led Zeppelin - Kashmir.mp4',
-            'Led Zeppelin - Who cares.mp4',
-            'Beatles - Who cares.mp4',
-            'Yes - We care.mp4',
+            'Led Zeppelin - Kashmir.mp3',
+            'Led Zeppelin - Who cares.mp3',
+            'Beatles - Who cares.mp3',
+            'Yes - We care.mp3',
             'Koolaid Man - Makes Music Now.mp3',
             'Koolaid Man - Makes Music Now.mp3',
             'Koolaid Man - Makes Music Now.mp3',
@@ -21,41 +21,98 @@ music_names = [
             'Hell Man - Makes Music Now.mp3',
             'Heaven Man - Makes Music Now.mp3',]
 
+
+play_list_bank = [
+            'So Strange - The Jesters.mp3', 
+            'Mama Guitar - Julius LaRosa.mp3',
+            'Say It Isnt So - Teddi King.mp3',
+            'Next In Line - Johnny Cash.mp3']
+
 random_categories = [
     'Jazz',
     'WhatEver',
     'Who Cares',
     'Jesus Music']
 
+
+def is_ascii(s):
+    return all(ord(c) < 128 for c in s)
+
+def check_list_for_non_ascii(string_list):
+    for string in string_list:
+        if not is_ascii(string):
+            print string
+            return False
+    return True
+
+
 class TestMusicOrganiztion(unittest.TestCase):
     def setUp(self):
-
-        
-        # Create temp test folder
         if os.path.exists('test'):
-            shutil.rmtree('test')
+            shutil.rmtree('test', ignore_errors=True)
         os.mkdir('test')
+        shutil.copy('test-list.txt', 'test')
         os.chdir('test')
-        for category in random_categories:
-            os.makedirs('unorganized_files/%s' % category)
-        
-        for name in music_names:
-            open('unorganized_files/%s/%s' % (random_categories[random.randint(0,3)], name), 'w')
 
     def tearDown(self):
-        pass
+        os.chdir('../')
+        shutil.rmtree('test', ignore_errors=True)
 
 
 
 
     def test_organization(self):
         print "after Organization list dir:"
-
-        music_organizer.organize_files_by_artist('unorganized_files', 'organized_files')
-        print os.listdir('organized_files')    
+        # Create temp test folder
+        for category in random_categories:
+            os.makedirs('unorganized_files/%s' % category)
+        
+        for name in music_names:
+            open('unorganized_files/%s/%s' % (random_categories[random.randint(0,3)], name), 'w')
+        music_organizer.organize_files_by_artist('unorganized_files', 'organized_files')    
         self.assertTrue('Beatles' in os.listdir('organized_files'))
         self.assertTrue('Led Zeppelin' in os.listdir('organized_files'))
         self.assertTrue('Yes' in os.listdir('organized_files'))
+
+    # def test_playlist_creator(self):
+    #     if os.path.exists('test_playlist_creator'):
+    #         shutil.rmtree('test_playlist_creator')
+    #     os.mkdir('test_playlist_creator')
+    #     os.chdir('test_playlist_creator')
+
+    def test_list_from_csv(self):
+        csv_file_name = 'test-list.txt'
+        playlist_files = music_organizer.create_list_from_csv(csv_file_name)
+        self.assertTrue('Mama Guitar - Julius LaRosa.mp3' in playlist_files)
+        self.assertTrue('Say It Isnt So - Teddi King.mp3' in playlist_files)
+        self.assertTrue('Next In Line - Johnny Cash.mp3' in playlist_files)
+        self.assertTrue('So Strange - The Jesters.mp3' in playlist_files)
+        self.assertFalse(' - .mp3' in playlist_files)
+        self.assertFalse('' in playlist_files)
+        self.assertFalse(' ' in playlist_files)
+        self.assertFalse(' .mp3' in playlist_files)
+
+
+    def test_add_files_to_playlist_folder(self):
+        os.mkdir('play-list-bank')
+
+        for name in play_list_bank:
+            open('play-list-bank/%s' % name, 'w')
+
+        csv_file_name = 'test-list.txt'
+        src = 'play-list-bank'
+        dst = 'dest-play-list'
+        missing_music_list = music_organizer.create_playlist_from_bank(csv_file_name, src, dst)
+        self.assertTrue('So Stranger - The Jesterers.mp3' in missing_music_list)
+        self.assertTrue('In Line - Johnny Cash.mp3' in missing_music_list)
+        self.assertTrue('Mama Guitar - Julius LaRosa.mp3' in os.listdir(dst))
+        self.assertTrue('Say It Isnt So - Teddi King.mp3' in os.listdir(dst))
+        self.assertTrue('Next In Line - Johnny Cash.mp3' in os.listdir(dst))
+        self.assertTrue('So Strange - The Jesters.mp3' in os.listdir(dst))
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
